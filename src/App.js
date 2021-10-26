d55f0316d7001109d45d49e9c64dab800c742c4a
FinndeJong <finn.dejong@student.hu.nl>
2021-10-26 10:45:09 +0200

    Save file voor Nav migratie

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+/* Import React */
 import './App.css';
+
+/* Import Components */
 import WebTemplate from './components/webtemplate/WebTemplate.js'
+
+/* Import Dependency */
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
 	return (
 		<div>
+			<Switch>
+				<Route>
+
+				</Route>
+				<Route>
+				
+				</Route>
+				<Route>
+				
+				</Route>
+				<Route>
+				
+				</Route>
+			</Switch>
 			<WebTemplate />
     	</div>
 	);
