e71c44d5f69bed09a89360ca690ac6d37d1cf9be
FinndeJong <finn.dejong@student.hu.nl>
2022-01-26 03:40:52 +0100

    Dynamic Grid

--- src/components/AddonSettings/AddonSettingsContent.js
+++ src/components/AddonSettings/AddonSettingsContent.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Card, Image, Row, Col } from 'react-bootstrap'
 
+// Helpers
+import { useDynamicGrid } from '../../helpers/useDynamicGrid.js';
+
 //components
 import AddonSettingsOption1 from '../addonSettings/AddonSettingsOption1.js';
 import AddonSettingsOption2 from '../addonSettings/AddonSettingsOption2.js';
@@ -14,11 +17,16 @@ import AddonSettingsSaveButton from './AddonSettingsSaveButton.js';
 
 function ProfilePageContent() {
 
+	let dummyarray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+
+	let rows = useDynamicGrid(dummyarray, 6);
+
     return (
         <>
             <Card className="addon-settings-card">
                 <Card.Body className="addon-settings-card-body">
-                    <Row className="setting-option-component-margin-row">
+                    {/* <Row className="setting-option-component-margin-row">
                         <Col className="setting-option-component-margin-col">
                             <AddonSettingsOption1 />
                         </Col>
@@ -38,8 +46,21 @@ function ProfilePageContent() {
                         <Col className="setting-option-component-margin-col">
                             <AddonSettingsOption5 />
                         </Col>
-                    </Row>
-                    <Row>
+                    </Row> */}
+
+					{Object.keys(rows).map(row => {
+						return (
+							<Row className="setting-option-component-margin-row" key={row}>
+								{rows[row].map(item => {
+									return (
+										<Col className="setting-option-component-margin-col">{item}</Col>
+									)
+								})}
+							</Row>
+						);
+					})}
+
+                    <Row className="setting-save-button">
                         <AddonSettingsSaveButton />
                     </Row>
                 </Card.Body>
