fb124c50a32decf20237c49dcaa7c6b060356562
FinndeJong <70587533+FinndeJong@users.noreply.github.com>
2022-01-28 17:11:14 +0100

    comment

--- src/components/AddonSettings/AddonSettingsContent.js
+++ src/components/AddonSettings/AddonSettingsContent.js
@@ -17,7 +17,7 @@ import AddonSettingsSaveButton from './AddonSettingsSaveButton.js';
 
 function ProfilePageContent({currentaddonsetting}) {
 
-	let dummyarray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+	// let dummyarray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 	let rows = useDynamicGrid(currentaddonsetting, 2);
 
