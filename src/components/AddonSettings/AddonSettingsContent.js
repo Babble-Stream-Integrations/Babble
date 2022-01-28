17d8932cb947a7861c784ced16026d04782d1feb
FinndeJong <70587533+FinndeJong@users.noreply.github.com>
2022-01-28 19:23:20 +0100

    naam verandering

--- src/components/AddonSettings/AddonSettingsContent.js
+++ src/components/AddonSettings/AddonSettingsContent.js
@@ -15,7 +15,7 @@ import AddonSettingsSaveButton from './AddonSettingsSaveButton.js';
 //style
 
 
-function ProfilePageContent({currentaddonsetting}) {
+function AddonSettingsContent({ currentaddonsetting }) {
 
 	// let dummyarray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
@@ -46,4 +46,4 @@ function ProfilePageContent({currentaddonsetting}) {
     )
 }
 
-export default ProfilePageContent
+export default AddonSettingsContent
