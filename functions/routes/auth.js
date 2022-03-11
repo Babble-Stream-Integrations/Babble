3afd206710410c425897678303c4e7b2f34a4682
JoJ3o <42371079+JoJ3o@users.noreply.github.com>
2022-03-11 08:35:36 +0100

    ok

--- functions/routes/auth.js
+++ functions/routes/auth.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const youtubeAuth = require('../services/youtubeAuth');
-const twitchAuth = require('../services/twitchAuth');
-const { initializeApp, getApps, getApp } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
+const express = require("express");
+const youtubeAuth = require("../services/youtubeAuth");
+const twitchAuth = require("../services/twitchAuth");
+const { initializeApp, getApps, getApp } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 
 getApps().length === 0 ? initializeApp() : getApp();
 const db = getFirestore();
@@ -10,25 +10,26 @@ const db = getFirestore();
 const router = express.Router();
 
 // Youtube routes
-router.get('/youtube/auth', (req, res) => {
-	youtubeAuth.getCode(res);
+router.get("/youtube/auth", (req, res) => {
+  youtubeAuth.getCode(res);
 });
 
-router.get('/youtube/callback', (req, res) => {
-	const {code} = req.query;
-	youtubeAuth.getTokensWithCode(code);
-	res.redirect('http://localhost:3000/rafflesettings');
+router.get("/youtube/callback", (req, res) => {
+  const { code } = req.query;
+  youtubeAuth.getTokensWithCode(code);
+  res.redirect("http://localhost:3000/rafflesettings");
 });
 
 // Twitch routes
-router.get('/twitch/auth', (req, res) => {
-	twitchAuth.getCode(res);
+router.get("/twitch/auth", (req, res) => {
+  twitchAuth.getCode(res);
 });
 
-router.get('/twitch/callback', (req, res) => {
-	const code = req.query;
-	twitchAuth.getTokensWithCode(res, code);
-	res.redirect('http://localhost:3000/rafflesettings');
+router.get("/twitch/callback", (req, res) => {
+  const code = req.query;
+  console.log(code);
+  twitchAuth.getTokensWithCode(res, code);
+  res.redirect("http://localhost:3000/rafflesettings");
 });
 
 module.exports = router;
