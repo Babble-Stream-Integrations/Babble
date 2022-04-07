a2e226196748725f67c5f6b93d9e9004b47bb366
Blamant <ACRBvanSomeren@gmail.com>
2022-04-07 14:33:59 +0200

    promise only

--- functions/services/spotifyApi.js
+++ functions/services/spotifyApi.js
@@ -1,15 +1,50 @@
 const axios = require('axios');
+const qs = require('qs');
 
-const spotifyApiKey = "BQBrJbRCPrChtdETCip4GeBewJwmecPlA-Wc9QgKTXDZcIeOSKUl5zb7_NVYR7yMIhuNKGGpBg99Ki5fcBW3J-5sLRJLZgBncxyp7qUBDcRta8EArlSuYuu6ndYL-cq_EpApebmKLVV-GZyGfLJ0aV7CHJEb3dMa-9oukNaAsWD8rVvGsoTSxI8"
+const client_id = "f817d73abc5c47e9b9af069bca544631"
+const client_secret = "a32e61b0576d4cf691c90eaf3f3d2016"
 
-async function getSpotifyCall() {
-    try {
-        const testApiCall = `https://api.spotify.com/v1`
-        const response = await axios.get(testApiCall);
-        console.log(response);
-    } catch (error) {
-        console.log(error);
+const auth_token = Buffer.from(`${client_id}:${client_secret}`, 'utf-8').toString('base64'); //Combine ClientID and ClientSecret for the auth token
+
+const getAuth = async () => {
+    try{
+      //make post request to SPOTIFY API for access token, sending relavent info
+      const token_url = 'https://accounts.spotify.com/api/token';
+      const data = qs.stringify({'grant_type':'client_credentials'});
+  
+      const response = await axios.post(token_url, data, {
+        headers: { 
+          'Authorization': `Basic ${auth_token}`,
+          'Content-Type': 'application/x-www-form-urlencoded' 
+        }
+      })
+      //return access token
+      return response.data.access_token;
+      //console.log(response.data.access_token);   
+    }catch(error){
+      //on fail, log the error in console
+      console.log(error);
     }
-}
-
-getSpotifyCall()
+  }
+  
+  const getAudioFeatures_Track = async (track_id) => {
+    //request token using getAuth() function
+    const access_token = await getAuth();
+    //console.log(access_token);
+  
+    const api_url = `https://api.spotify.com/v1/audio-features/${track_id}`;
+    //console.log(api_url);
+    try{
+      const response = await axios.get(api_url, {
+        headers: {
+          'Authorization': `Bearer ${access_token}`
+        }
+      });
+      //console.log(response.data);
+      return response.data;
+    }catch(error){
+      console.log(error);
+    }  
+  };
+  
+  console.log(getAudioFeatures_Track('07A0whlnYwfWfLQy4qh3Tq'));
